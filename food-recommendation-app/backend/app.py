197296f625f2339108095b834b1e8eb2bf861743
Tara Dehdari <tara.dehdari@gmail.com>
2024-12-03 20:17:38 -0800

    fixed host and port

--- food-recommendation-app/backend/app.py
+++ food-recommendation-app/backend/app.py
@@ -172,6 +172,6 @@ def form():
     return render_template('form-inputs.html')
 
 # Run the Flask app
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use PORT environment variable if available
-    app.run(host="0.0.0.0", port=port, debug=True)  # Bind to 0.0.0.0 for external access
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
